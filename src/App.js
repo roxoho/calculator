fec5123838a2365f8af427085d38b981dc72a8db
roxoho <as4372056@gmail.com>
2023-12-14 17:40:47 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
+
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  function display(value){
+    setInputValue(inputValue + value);
+  }
+  function calculate(){
+    let answers = eval(inputValue);
+    setInputValue(answers);
+  }
+  function clear(){
+    setInputValue("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form name="calc" className="calculator">
+      
+      <input type="text" className="value" value={inputValue}/>
+      <span  className="num clear" onClick={()=> clear()}>c</span >
+      <span  onClick={()=>display("/")}>/</span >
+      <span  onClick={()=>display("*")}>*</span >
+      <span  onClick={()=>display("7")}>7</span >
+      <span  onClick={()=>display("8")}>8</span >
+      <span  onClick={()=>display("9")}>9</span >
+      <span  onClick={()=>display("-")}>-</span >
+      <span  onClick={()=>display("4")}>4</span >
+      <span  onClick={()=>display("5")}>5</span >
+      <span  onClick={()=>display("6")}>6</span >
+      <span  onClick={()=>display("+")} className="plus">+</span >
+      <span  onClick={()=>display("1")}>1</span >
+      <span  onClick={()=>display("2")}>2</span >
+      <span  onClick={()=>display("3")}>3</span >
+      <span  onClick={()=>display("0")}>0</span >
+      <span  onClick={()=>display("00")}>00</span >
+      <span  onClick={()=>display(".")}>.</span >
+      <span  className="num equal" onClick={()=>calculate()}>=</span >
+    </form>
+    </>
   );
 }
 
